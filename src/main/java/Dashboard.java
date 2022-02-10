4f5549ad370d8abc4ba622aabe370dc44110b81a
amakaj <tmakaj213@gmail.com>
2022-02-09 23:27:29 -0500

    changed project structure

--- src/main/java/Dashboard.java
+++ src/main/java/Dashboard.java
@@ -1,16 +1,17 @@
+package main.java;
+
 import javax.swing.*;
 
 public class Dashboard {
-    /**
+    /*
+     * /**
      * Create the GUI and show it. For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
-     * 
+     *
      *
      */
-    private static void createAndShowGUI() {
-        JFrame.setDefaultLookAndFeelDecorated(true);
-
+    public void createAndShowGUI() {
         // Frame creation
         JFrame frame = new JFrame("jBull");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,6 +20,17 @@ public class Dashboard {
 
         // Display the window.
         frame.setVisible(true);
+
+        /* Set the menu bar */
+        JMenuBar menubar = new JMenuBar();
+        frame.setJMenuBar(menubar);
+
+        JLabel label1 = new JLabel();
+        ImageIcon image1 = new ImageIcon(getClass().getResource("./resources/bull.png"));
+        // .getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT)));
+
+        label1.setIcon(image1);
+        menubar.add(label1);
     }
 
     public static void main(String[] args) {
@@ -26,7 +38,8 @@ public class Dashboard {
         // creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI();
+                Dashboard d1 = new Dashboard();
+                d1.createAndShowGUI();
             }
         });
     }
