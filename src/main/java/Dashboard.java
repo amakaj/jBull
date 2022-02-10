c50a6dfdb3e7c56e61d835848e4d4b47bd20ae0e
amakaj <tmakaj213@gmail.com>
2022-02-10 02:14:39 -0500

    added menu bar with icons

--- src/main/java/Dashboard.java
+++ src/main/java/Dashboard.java
@@ -1,38 +1,73 @@
 package main.java;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Dashboard {
+    private void changeButtonStyle(JButton button) {
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(true);
+        button.setBackground(Color.WHITE);
+    }
+
     /*
-     * /**
-     * Create the GUI and show it. For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     *
-     *
+     * Method invoked from the event-dispatching thread for thread-safety,
+     * since oftentimes Java Swing elements are not thread-safe
+     * This method will create the GUI and show it.
      */
-    public void createAndShowGUI() {
+    private void createAndShowGUI() {
         // Frame creation
         JFrame frame = new JFrame("jBull");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(900, 600);
         frame.setResizable(false);
 
-        // Display the window.
+        // Display the window
         frame.setVisible(true);
 
-        /* Set the menu bar */
+        // Set the menu bar
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
+        menubar.setBackground(Color.WHITE); // Menu bar will be white
 
-        JLabel label1 = new JLabel();
-        ImageIcon image1 = new ImageIcon(getClass().getResource("./resources/bull.png"));
-        // .getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT)));
+        // Instantiate the image objects
+        ImageIcon bullIcon = new ImageIcon(new ImageIcon(getClass().getResource("bull.png")).getImage()
+                .getScaledInstance(25, 25, Image.SCALE_DEFAULT));
 
-        label1.setIcon(image1);
-        menubar.add(label1);
+        ImageIcon profileIcon = new ImageIcon(new ImageIcon(getClass().getResource("profile.png")).getImage()
+                .getScaledInstance(25, 25, Image.SCALE_DEFAULT));
+
+        // Set the icon image for the frame
+        frame.setIconImage(bullIcon.getImage());
+
+        // Create labels that just contain icons without text
+        JLabel bullIconLabel = new JLabel(bullIcon);
+        JLabel profileIconLabel = new JLabel(profileIcon);
+
+        // Menu items aligned on the left
+        JButton buySell = new JButton("Buy/Sell");
+        JButton news = new JButton("News");
+        JButton help = new JButton("Help");
+
+        changeButtonStyle(buySell);
+        changeButtonStyle(news);
+        changeButtonStyle(help);
+
+        menubar.add(bullIconLabel);
+        menubar.add(buySell);
+        menubar.add(news);
+        menubar.add(help);
+
+        // Menu items aligned on the right
+        menubar.add(Box.createHorizontalGlue()); // Separates right and left
+        JMenu profileName = new JMenu("John Doe");
+
+        menubar.add(profileName);
+        menubar.add(profileIconLabel);
     }
 
+    // Main function
     public static void main(String[] args) {
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
